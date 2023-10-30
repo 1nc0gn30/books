8972b060cb28877a5d3e4c706cd2e33c18addc70
Lionelthebuilder <younglionpics@gmail.com>
2023-10-30 00:55:05 -0400

    trying to configure for netlify launch test
    Signed-off-by: Lionelthebuilder <younglionpics@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
-const path = require('path')
+const path = require('path');
+const webpack = require('webpack'); 
+const dotenv = require('dotenv').config({path: __dirname + '/.env'});
 
-    module.exports = {
-        mode: 'development',
-        entry: './src/index.js',
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'bundle.js'
-        },devServer: {
-            contentBase: path.join(__dirname, 'dist'),
-            compress: true,
-            port: 9000
-        },
-        watch: true
-
-    }
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    watch: true,
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': JSON.stringify(dotenv.parsed)
+        }),
+    ],
+};
