56805c6181524b5b44fa8229c57ea4b735b10a06
neo <neo@localhost.localdomain>
2023-10-31 19:21:28 -0400

    gitignore update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-const webpack = require('webpack'); 
-const dotenv = require('dotenv').config({path: __dirname + '/.env'});
+const webpack = require('webpack');
+const dotenv = require('dotenv').config({ path: __dirname + '/.env' });
 
 module.exports = {
     mode: 'development',
@@ -10,11 +10,12 @@ module.exports = {
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
         compress: true,
         port: 9000
     },
-    watch: true,
     plugins: [
         new webpack.DefinePlugin({
             'process.env': JSON.stringify(dotenv.parsed)
